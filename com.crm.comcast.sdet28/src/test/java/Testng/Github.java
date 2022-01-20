4b9f3701f066a8a3d27a7e0c4bf583c5a815a2e1
VIKASH PRATAP SINGH <VIKASH PRATAP SINGH@DESKTOP-P0DJ7MS>
2022-01-20 22:45:15 +0530

    Method edit

--- com.crm.comcast.sdet28/src/test/java/Testng/Github.java
+++ com.crm.comcast.sdet28/src/test/java/Testng/Github.java
@@ -5,8 +5,11 @@ public class Github {
 	public static void main(String[] args) {
 		int a=4;
 		int b=5;
-		int sum=a+b;
+		int sum =a+b;
+			
+		
 		System.out.println("sum a +b");
+		System.out.println("sum of number is"+ sum);
 	}
 
 }
