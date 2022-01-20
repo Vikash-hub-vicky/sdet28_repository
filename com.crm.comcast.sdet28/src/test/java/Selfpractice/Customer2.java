c9b931c45b823c0bb7cebfea7be6fe04612a18b4
Anuj Prajapati <Anuj Prajapati@Anuj>
2022-01-21 02:18:22 +0530

    kumar Added

--- com.crm.comcast.sdet28/src/test/java/Selfpractice/Customer2.java
+++ com.crm.comcast.sdet28/src/test/java/Selfpractice/Customer2.java
@@ -19,7 +19,7 @@ public void customerTest7() {
 public void cutomerTest8() {
 System.out.println("This is reggression");
 	System.out.println("Anuj kumar test");
-	System.out.println("Vikas Singh");
+	System.out.println("Vikas Singh kumar");
 }
 
  	
